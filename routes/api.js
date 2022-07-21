fbecf2cdd3ed190127f115055bb6a8d590d0a016
Robin Ostrowski <104393956+Bin-Ostrowski@users.noreply.github.com>
2022-07-20 21:28:48 -0700

    added id to new notes

--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,24 @@
 const router = require('express').Router()
 const { readFile, appendToJson } =  require('../utils')
+var uniqid = require('uniqid');
 
+
+// GET all notes
 router.get('/notes', (req, res)=>{
 readFile('./db/db.json').then((data)=> res.json(JSON.parse(data)))
 })
 
+// POST new notes
 router.post('/notes', (req, res)=>{
-appendToJson(req.body, './db/db.json')
-res.json('note has been added')
-})
+    //give unique Id
+    req.body.id = uniqid();
+    //add to notes array
+    appendToJson(req.body, './db/db.json')
+    res.json(console.log ('note has been added'))
+});
+
+// Delete notes
+
+
 
 module.exports = router;
